1d1682a0e3df929bd5b0c9cfb4c664b39773b6d8
Konstantin <Gamanov@skbkontur.ru>
2019-09-30 20:09:34 +0500

    SomeRefactoring + run up

--- ULearn/Генерация текстов/FrequencyAnalysisTask.cs
+++ ULearn/Генерация текстов/FrequencyAnalysisTask.cs
@@ -6,35 +6,46 @@ namespace TextAnalysis
 	{
 		public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
 		{
-			var dictionaryCounter = new Dictionary<string, Dictionary<string, int>>();
-			var returnedValue = new Dictionary<string, string>();
-
-			foreach (var sentence in text)
+			var nGrammsCounter = new Dictionary<string, Dictionary<string, int>>();
+			
+			foreach (var sentences in text)
 			{
-				var counter = 1;
-				foreach (var word in sentence)
-				{
-					if (counter < sentence.Count)
-						NGrammCounter(dictionaryCounter, word, sentence[counter]);
-					counter++;
-				}
+				GetBigrams(sentences, nGrammsCounter);
 
-				var n = 0;
-				while (n + 2 < sentence.Count)
-				{
-					var twoWordsKey = sentence[n] + " " + sentence[n + 1];
-					var thirdWordValue = sentence[n + 2];
-					if (n + 2 < sentence.Count)
-						NGrammCounter(dictionaryCounter, twoWordsKey, thirdWordValue);
-					n++;
-				}
-
-				returnedValue = GetFrequencyDictionary(dictionaryCounter);
+				GetTrigrams(sentences, nGrammsCounter);
 			}
 
-			return returnedValue;
+			return GetFrequencyDictionary(nGrammsCounter);
 		}
-		private static void NGrammCounter(Dictionary<string, Dictionary<string, int>> dictionaryCounter, string twoWordsKey,
+
+		private static void GetBigrams(List<string> sentence,
+			Dictionary<string, Dictionary<string, int>> bigramCounter)
+		{
+			var counter = 1;
+			foreach (var words in sentence)
+			{
+				if (counter < sentence.Count)
+					NGrammCounter(bigramCounter, words, sentence[counter]);
+				counter++;
+			}
+		}
+
+		private static void GetTrigrams(List<string> sentence,
+			Dictionary<string, Dictionary<string, int>> trigramCounter)
+		{
+			var counter = 0;
+			while (counter + 2 < sentence.Count)
+			{
+				var twoWordsKey = sentence[counter] + " " + sentence[counter + 1];
+				var thirdWordValue = sentence[counter + 2];
+				if (counter + 2 < sentence.Count)
+					NGrammCounter(trigramCounter, twoWordsKey, thirdWordValue);
+				counter++;
+			}
+		}
+
+		private static void NGrammCounter(Dictionary<string, Dictionary<string, int>> dictionaryCounter,
+			string twoWordsKey,
 			string thirdWordValue)
 		{
 			if (dictionaryCounter.ContainsKey(twoWordsKey) &&
@@ -44,24 +55,36 @@ namespace TextAnalysis
 			}
 			else
 			{
-				if (dictionaryCounter.ContainsKey(twoWordsKey))
-				{
-					dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
-				}
-				else
-				{
-					dictionaryCounter.Add(twoWordsKey, new Dictionary<string, int>());
-					dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
-				}
+				AddNewValue(dictionaryCounter, twoWordsKey, thirdWordValue);
 			}
 		}
-		
+
+		private static void AddNewValue(Dictionary<string, Dictionary<string, int>> dictionaryCounter, string twoWordsKey, string thirdWordValue)
+		{
+			if (dictionaryCounter.ContainsKey(twoWordsKey))
+			{
+				dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
+			}
+			else
+			{
+				dictionaryCounter.Add(twoWordsKey, new Dictionary<string, int>());
+				dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
+			}
+		}
+
 		public static Dictionary<string, string> GetFrequencyDictionary(
 			Dictionary<string, Dictionary<string, int>> dictionaryCounter)
 		{
 			var maxValueOfChastota = 0;
 			var frequencyDictionary = new Dictionary<string, string>();
-			foreach (var key1 in dictionaryCounter)
+			foreach (var key1 in dictionaryCounter) maxValueOfChastota = 
+				SomeForeach(key1, frequencyDictionary, maxValueOfChastota);
+
+			return frequencyDictionary;
+		}
+
+		private static int SomeForeach(KeyValuePair<string, Dictionary<string, int>> key1, Dictionary<string, string> frequencyDictionary, int maxValueOfChastota)
+		{
 			foreach (var valueOfDictionaryCounter in key1.Value)
 				if (frequencyDictionary.ContainsKey(key1.Key))
 				{
@@ -82,7 +105,7 @@ namespace TextAnalysis
 					maxValueOfChastota = valueOfDictionaryCounter.Value;
 				}
 
-			return frequencyDictionary;
+			return maxValueOfChastota;
 		}
 	}
 }
