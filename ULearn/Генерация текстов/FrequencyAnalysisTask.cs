b0447c98281513d7cab3998e57a63ec78d933e77
Konstantin <Gamanov@skbkontur.ru>
2019-09-30 19:03:43 +0500

    Рефакторинг

--- ULearn/Генерация текстов/FrequencyAnalysisTask.cs
+++ ULearn/Генерация текстов/FrequencyAnalysisTask.cs
@@ -1,135 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace TextAnalysis
 {
-    internal static class FrequencyAnalysisTask
-    {
-        static int maxValueOfChastota = 0;
-        //static Dictionary<string, Dictionary<string, int>> dictionaryCounter = new Dictionary<string, Dictionary<string, int>>();
-        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
-        {
-            var dictionaryCounter = new Dictionary<string, Dictionary<string, int>>();
+	internal static class FrequencyAnalysisTask
+	{
+		private static int maxValueOfChastota = 0;
+		public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+		{
+			var dictionaryCounter = new Dictionary<string, Dictionary<string, int>>();
+			var returnedValue = new Dictionary<string, string>();
 
-            var returnedValue = new Dictionary<string, string>();
-            
-            //Выдели в отдельный метод с N граммами (а лучше в парочку - троечку)
-            foreach (var sentence in text)
-            {
-                var counter = 1;
-                foreach (var word in sentence)
-                {
-                    if (counter < sentence.Count)
-                    {
-                     //   NGrammCounter(word, sentence[counter]);
+			foreach (var sentence in text)
+			{
+				var counter = 1;
+				foreach (var word in sentence)
+				{
+					if (counter < sentence.Count)
+						NGrammCounter(dictionaryCounter, word, sentence[counter]);
+					counter++;
+				}
 
-                        //Считаем сколько раз повторились ключ + значение в тексте
-                        if (dictionaryCounter.ContainsKey(word) && dictionaryCounter[word].ContainsKey(sentence[counter]))
-                        {
-                            dictionaryCounter[word][sentence[counter]]++;
-                        }
-                        else
-                        {
-                            if (dictionaryCounter.ContainsKey(word))
-                            {
-                                dictionaryCounter[word].Add(sentence[counter], 1);
-                            }
-                            else
-                            {
-                                dictionaryCounter.Add(word, new Dictionary<string, int>());
-                                dictionaryCounter[word].Add(sentence[counter], 1);
-                            }
-						
-                        }
-                    }
-                    counter++;
-                }
-                
+				var n = 0;
+				while (n + 2 < sentence.Count)
+				{
+					var twoWordsKey = sentence[n] + " " + sentence[n + 1];
+					var thirdWordValue = sentence[n + 2];
+					if (n + 2 < sentence.Count)
+						NGrammCounter(dictionaryCounter, twoWordsKey, thirdWordValue);
+					n++;
+				}
 
-                
-                var n = 0;
-                while (n + 2 < sentence.Count)
-                {
-                    var twoWordsKey = sentence[n] + " " + sentence[n + 1];
-                    var thirdWordValue = sentence[n + 2];
-                    if (n + 2 < sentence.Count)
-                    {
-                        //NGrammCounter(twoWordsKey, thirdWordValue);
-                        //Считаем сколько раз повторились ключ + значение в тексте
-                        if (dictionaryCounter.ContainsKey(twoWordsKey) && dictionaryCounter[twoWordsKey].ContainsKey(thirdWordValue))
-                        {
-                            dictionaryCounter[twoWordsKey][thirdWordValue]++;
-                        }
-                        else
-                        {
-                            if (dictionaryCounter.ContainsKey(twoWordsKey))
-                            {
-                                dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
-                            }
-                            else
-                            {
-                                dictionaryCounter.Add(twoWordsKey, new Dictionary<string, int>());
-                                dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
-                            }
-                        }
-                    }
-				    n++;
-                }
-                
-                returnedValue = GetFrequencyDictionary(dictionaryCounter);
+				returnedValue = GetFrequencyDictionary(dictionaryCounter);
+			}
 
-            }
-            return returnedValue;
-        }
+			return returnedValue;
+		}
+		private static void NGrammCounter(Dictionary<string, Dictionary<string, int>> dictionaryCounter, string twoWordsKey,
+			string thirdWordValue)
+		{
+			if (dictionaryCounter.ContainsKey(twoWordsKey) &&
+			    dictionaryCounter[twoWordsKey].ContainsKey(thirdWordValue))
+			{
+				dictionaryCounter[twoWordsKey][thirdWordValue]++;
+			}
+			else
+			{
+				if (dictionaryCounter.ContainsKey(twoWordsKey))
+				{
+					dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
+				}
+				else
+				{
+					dictionaryCounter.Add(twoWordsKey, new Dictionary<string, int>());
+					dictionaryCounter[twoWordsKey].Add(thirdWordValue, 1);
+				}
+			}
+		}
+		
+		public static Dictionary<string, string> GetFrequencyDictionary(
+			Dictionary<string, Dictionary<string, int>> dictionaryCounter)
+		{
+			var frequencyDictionary = new Dictionary<string, string>();
+			foreach (var key1 in dictionaryCounter)
+			foreach (var valueOfDictionaryCounter in key1.Value)
+				if (frequencyDictionary.ContainsKey(key1.Key))
+				{
+					if (valueOfDictionaryCounter.Value > maxValueOfChastota)
+					{
+						maxValueOfChastota = valueOfDictionaryCounter.Value;
+						frequencyDictionary[key1.Key] = valueOfDictionaryCounter.Key;
+					}
+					else if (valueOfDictionaryCounter.Value == maxValueOfChastota &&
+					         string.CompareOrdinal(valueOfDictionaryCounter.Key, frequencyDictionary[key1.Key]) < 0)
+					{
+						frequencyDictionary[key1.Key] = valueOfDictionaryCounter.Key;
+					}
+				}
+				else
+				{
+					frequencyDictionary.Add(key1.Key, valueOfDictionaryCounter.Key);
+					maxValueOfChastota = valueOfDictionaryCounter.Value;
+				}
 
-//        public static void NGrammCounter(string externalKey, string internalKey)
-//        {
-//            if (dictionaryCounter.ContainsKey(externalKey) && dictionaryCounter[externalKey].ContainsKey(internalKey))
-//            {
-//                dictionaryCounter[externalKey][internalKey]++;
-//            }
-//            else
-//            {
-//                if (dictionaryCounter.ContainsKey(externalKey))
-//                {
-//                    dictionaryCounter[externalKey].Add(internalKey, 1);
-//                }
-//                else
-//                {
-//                    dictionaryCounter.Add(externalKey, new Dictionary<string, int>());
-//                    dictionaryCounter[externalKey].Add(internalKey, 1);
-//                }
-//            }
-//        }
-
-        public static Dictionary<string, string> GetFrequencyDictionary(Dictionary<string, Dictionary<string, int>> dictionaryCounter)
-        {
-            var frequencyDictionary = new Dictionary<string, string>();
-            foreach (var key1 in dictionaryCounter)
-            {
-                foreach (var valueOfDictionaryCounter in key1.Value)
-                {
-                    if (frequencyDictionary.ContainsKey(key1.Key))
-                    {
-
-                        if (valueOfDictionaryCounter.Value > maxValueOfChastota )
-                        {
-                            maxValueOfChastota = valueOfDictionaryCounter.Value;
-                            frequencyDictionary[key1.Key] = valueOfDictionaryCounter.Key; 
-                        }
-                        else if (valueOfDictionaryCounter.Value == maxValueOfChastota && (string.CompareOrdinal(valueOfDictionaryCounter.Key, frequencyDictionary[key1.Key]) < 0))
-                        {
-                            frequencyDictionary[key1.Key] = valueOfDictionaryCounter.Key;
-                        }
-                    }
-                    else
-                    {
-                        frequencyDictionary.Add(key1.Key, valueOfDictionaryCounter.Key);
-                        maxValueOfChastota = valueOfDictionaryCounter.Value;
-                    }
-                }
-            }
-            return frequencyDictionary;
-        }
-    }
+			return frequencyDictionary;
+		}
+	}
 }
