cd085e4d09520df0a34cbb46e710dd1a128b2947
Konstantin <Gamanov@skbkontur.ru>
2019-10-04 10:48:08 +0500

    Закончил рефачить, отдал на ревью

--- ULearn/Генерация текстов/TextGeneratorTask.cs
+++ ULearn/Генерация текстов/TextGeneratorTask.cs
@@ -4,87 +4,80 @@ using System.Text;
 
 namespace TextAnalysis
 {
-    internal static class TextGeneratorTask
-    {
-        public static string ContinuePhrase(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
-        {
-         
-            var words = new List<string>();
-            var remainingWordsNumber = 0;
-            words = phraseBeginning.Split().ToList();
+	internal static class TextGeneratorTask
+	{
+		public static string ContinuePhrase(Dictionary<string, string> nextWords,
+			string phraseBeginning, int wordsCount)
+		{
+			var words = new List<string>();
+			var remainingWordsNumber = 0;
 
-            if (words.Count == 1)
-            {
-                if (nextWords.ContainsKey(words[words.Count - 1]) && wordsCount != 0)
-                {
-                    AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, words[words.Count - 1], words);
-                    remainingWordsNumber++;
-                }
-                else
-                {
-                    return phraseBeginning;
-                }
-            }
-                        
-            AddContinuationOfTwoOrOneWord(nextWords, ref phraseBeginning, wordsCount, remainingWordsNumber, words);
+			words = phraseBeginning.Split().ToList();
 
-            
-            return phraseBeginning;
-        }
+			//Дон Костыльере
+			var continuePhrase = AddContinationOfOneWord(nextWords, ref phraseBeginning,
+				wordsCount, words, ref remainingWordsNumber);
+			if (continuePhrase != null) return continuePhrase;
+			//
 
-        private static void AddContinuationOfTwoOrOneWord(Dictionary<string, string> nextWords, ref string phraseBeginning, int wordsCount,
-            int remainingWordsNumber, List<string> words)
-        {
-            while (wordsCount > remainingWordsNumber)
-            {
-                var twoLastWordsOfPhrase = words[words.Count - 2] + ' ' + words[words.Count - 1];
-                var lastWordOfPhrase = words[words.Count - 1];
+			CheckingKeyAndAddWord(nextWords, ref phraseBeginning,
+				wordsCount, remainingWordsNumber, words);
 
-                if (nextWords.ContainsKey(twoLastWordsOfPhrase))
-                {
-                    AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, twoLastWordsOfPhrase, words);
-                }
-                else
-                {
-                    if (nextWords.ContainsKey(lastWordOfPhrase))
-                    {
-                        AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, lastWordOfPhrase, words);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+			return phraseBeginning;
+		}
 
-                remainingWordsNumber++;
-            }
-        }
+		private static string AddContinationOfOneWord(Dictionary<string, string> nextWords,
+			ref string phraseBeginning, int wordsCount, List<string> words,
+			ref int remainingWordsNumber)
+		{
+			if (words.Count == 1)
+			{
+				if (nextWords.ContainsKey(words[words.Count - 1]) && wordsCount != 0)
+				{
+					AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, words[words.Count - 1], words);
+					remainingWordsNumber++;
+				}
+				else
+				{
+					return phraseBeginning;
+				}
+			}
+
+			return null;
+		}
+
+		private static void CheckingKeyAndAddWord(Dictionary<string, string> nextWords, ref string phraseBeginning,
+			int wordsCount,
+			int remainingWordsNumber, List<string> words)
+		{
+			while (wordsCount > remainingWordsNumber)
+			{
+				var twoLastWordsOfPhrase = words[words.Count - 2] + ' ' + words[words.Count - 1];
+				var lastWordOfPhrase = words[words.Count - 1];
+
+				if (nextWords.ContainsKey(twoLastWordsOfPhrase))
+				{
+					AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, twoLastWordsOfPhrase, words);
+				}
+				else
+				{
+					if (nextWords.ContainsKey(lastWordOfPhrase))
+						AddTheWordToTheEndOfThePhrase(nextWords, ref phraseBeginning, lastWordOfPhrase, words);
+					else
+						break;
+				}
+
+				remainingWordsNumber++;
+			}
+		}
 
 
-        private static void AddTheWordToTheEndOfThePhrase(Dictionary<string, string> nextWords, ref string phraseBeginning,
-            string lastWord, List<string> words)
-        {
-            phraseBeginning = phraseBeginning + ' ' + nextWords[lastWord];
-            words.Add(nextWords[lastWord]);
-        }
-    }
+		private static void AddTheWordToTheEndOfThePhrase(Dictionary<string, string> nextWords,
+			ref string phraseBeginning,
+			string lastWord, List<string> words)
+		{
+			phraseBeginning = phraseBeginning + ' ' + nextWords[lastWord];
+			words.Add(nextWords[lastWord]);
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
