6555176fb34dd88b3c6ecdeeb59981c1bc468e6d
Gamanov Konstantin <gamanov@skbkontur.ru>
2021-03-05 16:53:09 +0500

    it's task

---
+++ ULearn/Земля и Диггер/DiggerTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digger
+{
+    public class Terrian : ICreature
+    {
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Player : ICreature
+    {
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    //Напишите здесь классы Player, Terrain и другие.
+
+   
+}
