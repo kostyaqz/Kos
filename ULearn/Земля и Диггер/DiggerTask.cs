1f7e09c41fc06854526bff49ae77553e3011c696
Gamanov <gamanov@skbkontur.ru>
2022-11-06 23:02:04 +0500

    Пытаюсь подобрать ограничения так, чтобы мешок не выходил за границы массива

--- ULearn/Земля и Диггер/DiggerTask.cs
+++ ULearn/Земля и Диггер/DiggerTask.cs
@@ -2,161 +2,168 @@
 
 namespace Digger
 {
-	//Земля
-	public class Terrain : ICreature
-	{
-		public string GetImageFileName()
-		{
-			return "Terrain.png";
-		}
+    //Земля
+    public class Terrain : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Terrain.png";
+        }
 
-		public int GetDrawingPriority()
-		{
-			return 0;
-		}
+        public int GetDrawingPriority()
+        {
+            return 0;
+        }
 
-		public CreatureCommand Act(int x, int y)
-		{
-			var creatureCommand = new CreatureCommand
-			{
-				DeltaX = 0,
-				DeltaY = 0,
-				TransformTo = null
-			};
-			return creatureCommand;
-		}
+        public CreatureCommand Act(int x, int y)
+        {
+            var creatureCommand = new CreatureCommand
+            {
+                DeltaX = 0,
+                DeltaY = 0,
+                TransformTo = null
+            };
+            return creatureCommand;
+        }
 
-		public bool DeadInConflict(ICreature conflictedObject)
-		{
-			return true;
-		}
-	}
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+    }
 
-	// Игрок, он же диггер
-	public class Player : ICreature
-	{
-		public string GetImageFileName()
-		{
-			return "Digger.png";
-		}
+    // Игрок, он же диггер
+    public class Player : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Digger.png";
+        }
 
-		public int GetDrawingPriority()
-		{
-			return 1;
-		}
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
 
-		public CreatureCommand Act(int x, int y)
-		{
-			var act = new CreatureCommand
-			{
-				DeltaX = 0,
-				DeltaY = 0,
-				TransformTo = null
-			};
-			var key = Game.KeyPressed;
+        public CreatureCommand Act(int x, int y)
+        {
+            var act = new CreatureCommand
+            {
+                DeltaX = 0,
+                DeltaY = 0,
+                TransformTo = null
+            };
+            var key = Game.KeyPressed;
 
-			if (key == Keys.Left && x > 0)
-				act.DeltaX = -1;
-			else if (key == Keys.Right && x < Game.MapWidth - 1)
-				act.DeltaX = 1;
-			else if (key == Keys.Up && y > 0)
-				act.DeltaY = -1;
-			else if (key == Keys.Down && y < Game.MapHeight - 1) 
-				act.DeltaY = 1;
-			return act;
-		}
+            if (key == Keys.Left && x > 0)
+                act.DeltaX = -1;
+            if (key == Keys.Right && x < Game.MapWidth - 1)
+                act.DeltaX = 1;
+            if (key == Keys.Up && y > 0)
+                act.DeltaY = -1;
+            if (key == Keys.Down && y < Game.MapHeight - 1)
+                act.DeltaY = 1;
+            return act;
+        }
 
-		public bool DeadInConflict(ICreature conflictedObject)
-		{
-			return false;
-		}
-	}
-	
-	// Мешок с золотом
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+    }
 
-	public class Sack : ICreature
-	{
-		public string GetImageFileName()
-		{
-			return "Sack.png";
-		}
+    // Мешок с золотом
 
-		public int GetDrawingPriority()
-		{
-			return 10;
-		}
+    public class Sack : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Sack.png";
+        }
 
-		public CreatureCommand Act(int x, int y)
-		{
-			var act = new CreatureCommand()
-			{
-				DeltaX = 0,
-				DeltaY = 0,
-				TransformTo = null
-			}; 
-			
-			//Game.Map[x, y].toString() == "<имя файла>.png" или Game.Map (is или ==) <имя объекта>
+        public int GetDrawingPriority()
+        {
+            return 10;
+        }
 
-			var nextPosition = act;
-			
-			if (y + 1 < Game.MapHeight && Game.Map[x, y + 1].ToString() == "Digger.png")
-				act.DeltaY = 1;
+        public CreatureCommand Act(int x, int y)
+        {
+            var act = new CreatureCommand()
+            {
+                DeltaX = 0,
+                DeltaY = 0,
+                TransformTo = null
+            };
 
-			return act;
-		}
+            //Game.Map[x, y].toString() == "<имя файла>.png" или Game.Map (is или ==) <имя объекта>
 
-		public bool DeadInConflict(ICreature conflictedObject)
-		{
-			return true;
-		}
-	}
-	
-	// Золото
+            var nextPosition = act;
+            //var map = Game.Map[x, y + 1];
 
-	public class Gold : ICreature
-	{
-		public string GetImageFileName()
-		{
-			return "Gold.png";
-		}
+            if (Game.KeyPressed == Keys.Up && !Block.IsYBorder(y) && !Block.IsBarrier(x, y))
+                act.DeltaY = 1;
 
-		public int GetDrawingPriority()
-		{
-			return 2;
-		}
+            return act;
+        }
 
-		public CreatureCommand Act(int x, int y)
-		{
-			var act = new CreatureCommand()
-			{
-				DeltaX = 0,
-				DeltaY = 0,
-				TransformTo = null
-			};
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+    }
 
-			return act;
-		}
+    // Золото
 
-		public bool DeadInConflict(ICreature conflictedObject)
-		{
-			return true;
-		}
-	}
-	
-	
-	//Обработка движений должна выехать отдельно
-	// static Movements
-	
-	//Обработка границ карты должна выехать отдельно
-	// public static class GameExtension
-	// {
-	// 	private static Boundary boundary = new Boundary();
-	// 	public static ICreature GetCreature(int x, int y)
-	// 	{
-	// 		if (x < 0 || y < 0 || x >= Game.MapWidth || y >= Game.MapHeight)
-	// 			return boundary;
-	// 		return Game.Map[x, y];
-	// 	}
-	// }
-	
+    public class Gold : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Gold.png";
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 2;
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            var act = new CreatureCommand()
+            {
+                DeltaX = 0,
+                DeltaY = 0,
+                TransformTo = null
+            };
+
+            return act;
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+    }
+
+
+    //Обработка движений должна выехать отдельно
+    // static Movements
+
+    //Класс для падения мешка
+    public class Block
+    {
+        public static bool IsYBorder(int y)
+        {
+            return y < Game.MapHeight - 1;
+        }
+        public static bool IsBarrier(int x, int y)
+        {
+            if (Game.Map[x, y + 1] is Player)
+                return true;
+            if (Game.Map[x, y + 1] is Gold)
+                return true;
+            if (Game.Map[x, y + 1] is Terrain)
+                return true;
+            return false;
+        }
+    }
 }
