6ae38ef5497c1d005c052e5784551d39616b6712
Gamanov Konstantin <gamanov@skbkontur.ru>
2022-11-06 14:21:21 +0500

    Игра запустилась за счет новых классов, но нормально пока не работает

--- ULearn/Земля и Диггер/DiggerTask.cs
+++ ULearn/Земля и Диггер/DiggerTask.cs
@@ -2,6 +2,7 @@
 
 namespace Digger
 {
+	//Земля
 	public class Terrain : ICreature
 	{
 		public string GetImageFileName()
@@ -31,6 +32,7 @@ namespace Digger
 		}
 	}
 
+	// Игрок, он же диггер
 	public class Player : ICreature
 	{
 		public string GetImageFileName()
@@ -69,4 +71,92 @@ namespace Digger
 			return false;
 		}
 	}
+	
+	// Мешок с золотом
+
+	public class Sack : ICreature
+	{
+		public string GetImageFileName()
+		{
+			return "Sack.png";
+		}
+
+		public int GetDrawingPriority()
+		{
+			return 10;
+		}
+
+		public CreatureCommand Act(int x, int y)
+		{
+			var act = new CreatureCommand()
+			{
+				DeltaX = 0,
+				DeltaY = 0,
+				TransformTo = null
+			}; 
+			
+			//Game.Map[x, y].toString() == "<имя файла>.png" или Game.Map (is или ==) <имя объекта>
+
+			var nextPosition = act;
+			
+			if (y + 1 < Game.MapHeight && Game.Map[x, y + 1].ToString() == "Digger.png")
+				act.DeltaY = 1;
+
+			return act;
+		}
+
+		public bool DeadInConflict(ICreature conflictedObject)
+		{
+			return true;
+		}
+	}
+	
+	// Золото
+
+	public class Gold : ICreature
+	{
+		public string GetImageFileName()
+		{
+			return "Gold.png";
+		}
+
+		public int GetDrawingPriority()
+		{
+			return 2;
+		}
+
+		public CreatureCommand Act(int x, int y)
+		{
+			var act = new CreatureCommand()
+			{
+				DeltaX = 0,
+				DeltaY = 0,
+				TransformTo = null
+			};
+
+			return act;
+		}
+
+		public bool DeadInConflict(ICreature conflictedObject)
+		{
+			return true;
+		}
+	}
+	
+	
+	//Обработка движений должна выехать отдельно
+	// static Movements
+	
+	//Обработка границ карты должна выехать отдельно
+	// public static class GameExtension
+	// {
+	// 	private static Boundary boundary = new Boundary();
+	// 	public static ICreature GetCreature(int x, int y)
+	// 	{
+	// 		if (x < 0 || y < 0 || x >= Game.MapWidth || y >= Game.MapHeight)
+	// 			return boundary;
+	// 		return Game.Map[x, y];
+	// 	}
+	// }
+	
 }
