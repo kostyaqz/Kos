b54c1b87e7c0d34440d1b4c90dbf07b574029abd
Gamanov Konstantin <gamanov@skbkontur.ru>
2022-11-06 13:32:48 +0500

    Решил первую задачку. Отдал на ревью

--- ULearn/Земля и Диггер/DiggerTask.cs
+++ ULearn/Земля и Диггер/DiggerTask.cs
@@ -1,58 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
 
 namespace Digger
 {
-    public class Terrian : ICreature
-    {
-        public string GetImageFileName()
-        {
-            throw new NotImplementedException();
-        }
+	public class Terrain : ICreature
+	{
+		public string GetImageFileName()
+		{
+			return "Terrain.png";
+		}
 
-        public int GetDrawingPriority()
-        {
-            throw new NotImplementedException();
-        }
+		public int GetDrawingPriority()
+		{
+			return 0;
+		}
 
-        public CreatureCommand Act(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
+		public CreatureCommand Act(int x, int y)
+		{
+			var creatureCommand = new CreatureCommand
+			{
+				DeltaX = 0,
+				DeltaY = 0,
+				TransformTo = null
+			};
+			return creatureCommand;
+		}
 
-        public bool DeadInConflict(ICreature conflictedObject)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public bool DeadInConflict(ICreature conflictedObject)
+		{
+			return true;
+		}
+	}
 
-    public class Player : ICreature
-    {
-        public string GetImageFileName()
-        {
-            throw new NotImplementedException();
-        }
+	public class Player : ICreature
+	{
+		public string GetImageFileName()
+		{
+			return "Digger.png";
+		}
 
-        public int GetDrawingPriority()
-        {
-            throw new NotImplementedException();
-        }
+		public int GetDrawingPriority()
+		{
+			return 1;
+		}
 
-        public CreatureCommand Act(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
+		public CreatureCommand Act(int x, int y)
+		{
+			var act = new CreatureCommand
+			{
+				DeltaX = 0,
+				DeltaY = 0,
+				TransformTo = null
+			};
+			var key = Game.KeyPressed;
 
-        public bool DeadInConflict(ICreature conflictedObject)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    
-    //Напишите здесь классы Player, Terrain и другие.
+			if (key == Keys.Left && x > 0)
+				act.DeltaX = -1;
+			else if (key == Keys.Right && x < Game.MapWidth - 1)
+				act.DeltaX = 1;
+			else if (key == Keys.Up && y > 0)
+				act.DeltaY = -1;
+			else if (key == Keys.Down && y < Game.MapHeight - 1) 
+				act.DeltaY = 1;
+			return act;
+		}
 
-   
+		public bool DeadInConflict(ICreature conflictedObject)
+		{
+			return false;
+		}
+	}
 }
