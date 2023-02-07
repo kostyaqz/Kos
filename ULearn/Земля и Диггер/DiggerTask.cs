09a8f8b265eecfde1d6f2c7ff78d1839feb8aa02
Gamanov Konstantin <gamanov@skbkontur.ru>
2023-02-07 19:18:21 +0500

    Добавил логику того, как монстр ходит, но оно не работает. Надо копать какого хрена так происходит

--- ULearn/Земля и Диггер/DiggerTask.cs
+++ ULearn/Земля и Диггер/DiggerTask.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Digger
@@ -188,15 +190,21 @@ namespace Digger
                 return act;
             }
 
+            //todo тут логика того как монстр движется к человеку
 
+            var direction = MonsterWalking.MonsterDirection(x, y, playerPoint[0], playerPoint[1]);
 
-
-
-
-
-
-
-
+            switch (direction)
+            {
+                case "Left": act.DeltaX = -1;
+                    break;
+                case "Right": act.DeltaX = 1;
+                    break;
+                case "Up": act.DeltaY = -1;
+                    break;
+                case "Down": act.DeltaY = 1;
+                    break;
+            }
             return act;
         }
 
@@ -245,22 +253,51 @@ namespace Digger
         }
     }
 
-    public class WhoIsIt
+    public class MonsterWalking
     {
-        //Todo Пока сделал, чтобы он возвращал строку, но в идеале он должен возвращать сразу нужный класс.
-        public static string WhoIsForMonster(int x, int y)
+        public static bool CanMonsterGo(int x, int y)
         {
-            if (Game.Map[x, y] is Player)
+            if ((Game.Map[x, y] is Terrain) || (Game.Map[x, y] is Sack))
             {
-                return "Player";
+                return false;
             }
-            if (Game.Map[x, y] is Monster)
-            {
-                return "Monster";
-            }
-            //Todo. И так далее. Нужно дописать для всех сущностей + какое-то ожидаение, если попало что-то непонятное.
-            //Todo Пока ориентируюсь на то, что этот класс будет юзать монстр, так что и сущности тоже для него
-            return "";
+
+            return true;
         }
+
+        public static string MonsterDirection(int xMonster, int yMonster, int xPlayer, int yPlayer)
+        {
+            var distance = new Dictionary<double, string>();
+
+            //Todo надо разобраться с x и y. Кажется, что тут не то передается, что должно. Путаются x и y монстра и человека
+            if (Border.IsNoBorder(xMonster, yMonster, "Left") && CanMonsterGo(xMonster - 1, yMonster))
+            {
+                distance.Add(1000, "Left");
+            }
+            if (Border.IsNoBorder(xMonster, yMonster, "Right") && CanMonsterGo(xMonster + 1, yMonster))
+            {
+                distance.Add(1001, "Right");
+            }
+            if (Border.IsNoBorder(xMonster, yMonster, "Up") && CanMonsterGo(xMonster, yMonster - 1))
+            {
+                distance.Add(1002, "Up");
+            }
+            if (Border.IsNoBorder(xMonster, yMonster, "Down") && CanMonsterGo(xMonster, yMonster + 1))
+            {
+                distance.Add(1003, "Down");
+            }
+
+
+
+            foreach (var e in distance)
+            {
+                var oldValue = distance[e.Key];
+                distance.Remove(e.Key);
+                distance.Add(Math.Sqrt(Math.Pow(xPlayer - xMonster, 2) + Math.Pow(yPlayer - yMonster, 2)), oldValue);
+            }
+
+            return distance[distance.Keys.Min()];
+        }
+
     }
 }
