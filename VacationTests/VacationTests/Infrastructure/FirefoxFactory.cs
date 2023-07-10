ba979bde8d93b76f8e77414bc9999f2e081bfcd6
Gamanov Konstantin <gamanov@skbkontur.ru>
2023-07-10 17:57:29 +0500

    Задание 8

---
+++ VacationTests/VacationTests/Infrastructure/FirefoxFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Kontur.Selone.Extensions;
+using Kontur.Selone.WebDrivers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace VacationTests.Infrastructure
+{
+    public class FirefoxFactory : IWebDriverFactory
+    {
+        public IWebDriver Create()
+        {
+            var firefoxDriverService = FirefoxDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
+            var options = new FirefoxOptions();
+            options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+            var firefoxDriver = new FirefoxDriver(firefoxDriverService, options, TimeSpan.FromSeconds(180));
+            firefoxDriver.Manage().Window.SetSize(1280, 960);
+            return firefoxDriver;
+        }
+    }
+}
