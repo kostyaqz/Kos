271a10a1de90eaf1f2304afc4f6d2145d25b4d02
Gamanov Konstantin <gamanov@skbkontur.ru>
2023-07-17 15:33:37 +0500

    Подлил пулл браузеров

--- VacationTests/VacationTests/Infrastructure/MyBrowserPool.cs
+++ VacationTests/VacationTests/Infrastructure/MyBrowserPool.cs
@@ -1,7 +1,41 @@
-﻿namespace VacationTests.Infrastructure
+﻿using System.Collections.Concurrent;
+using Kontur.Selone.Extensions;
+using Kontur.Selone.WebDrivers;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace VacationTests.Infrastructure
 {
-    public class MyBrowserPool
+    public static class MyBrowserPool
     {
-        
+        static MyBrowserPool()
+        {
+            pool = new WebDriverPool(new ChromeDriverFactory(), cleaner);
+        }
+
+        static IWebDriverPool pool;
+
+
+        private static ConcurrentDictionary<string, IWebDriver> webDriversMap = new();
+        private static string key => TestContext.CurrentContext.Test.ID ?? "debug";
+
+        public static IWebDriver Get()
+        {
+            var browser = webDriversMap.GetOrAdd(key, _=> pool.Acquire());
+            return browser;
+        }
+
+        public static void Release()
+        {
+            if (webDriversMap.TryRemove(key, out var browser))
+                pool.Release(browser);
+        }
+
+        public static void Dispose()
+        {
+            pool.Clear();
+        }
+
+        private static DelegateWebDriverCleaner cleaner = new(x => x.ResetWindows());
     }
 }
